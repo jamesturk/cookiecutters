d17fc61c802dd4cca33dc21de42bbe49db07597e
James Turk <dev@jamesturk.net>
2023-12-25 05:35:33 -0600

    hooks

--- make_symlinks.py
+++ make_symlinks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
 """ Helper script that copies things from _common to the cookiecutter root """
 
+import os
 import pathlib
 
 cookies = ["core", "django"]
@@ -21,6 +21,6 @@ def symlink_all_files(from_dir, to_dir, symlink=True):
 
 
 symlink_all_files("_common", "{{cookiecutter.project_slug}}")
-pathlib.Path("core/hooks").mkdir(parents=True, exist_ok=True)
-pathlib.Path("django/hooks").mkdir(parents=True, exist_ok=True)
-symlink_all_files("_hooks", f"hooks", symlink=False)
+
+os.system("mkdir -p core/hooks && cp _hooks/* core/hooks/")
+os.system("mkdir -p django/hooks && cp _hooks/* django/hooks/")
