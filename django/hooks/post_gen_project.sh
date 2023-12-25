9d871a01b18e20bf7ce9c4e35075bd55bffce1ef
James Turk <dev@jamesturk.net>
2023-12-25 05:49:05 -0600

    fix postgen

---
+++ django/hooks/post_gen_project.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# the latest versions of our core packages
+poetry add --dev pytest pytest-cov mkdocs-material ruff mypy
+poetry add structlog
+poetry add {{ cookiecutter.extra_packages }}
+
+# initialize git repo and install pre-commit hooks
+git init
+git add .
+git commit -m "cookiecutter commit"
+pre-commit install
