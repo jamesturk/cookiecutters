568e25d9f4a58d673d7886ea9907ac406ece09c3
James Turk <dev@jamesturk.net>
2023-12-25 02:53:33 -0600

    one more

--- {{cookiecutter.project_slug}}/tests/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/test_cli.py
@@ -2,6 +2,7 @@ import importlib.metadata
 from typer.testing import CliRunner
 from {{cookiecutter.project_slug}}.cli import cli
 
+
 def test_version():
     runner = CliRunner()
     result = runner.invoke(cli, ["--version"])
