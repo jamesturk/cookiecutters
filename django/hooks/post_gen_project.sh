37053bb82635fdd5e1c53ff21c0b3e42313d8eda
James Turk <dev@jamesturk.net>
2023-12-25 03:16:27 -0600

    remove black, add django

--- django/hooks/post_gen_project.sh
+++ django/hooks/post_gen_project.sh
@@ -3,6 +3,6 @@
 # the latest versions of our core packages
 poetry add --dev pytest pytest-cov mkdocs-material ruff mypy
 poetry add structlog
-poetry add Django django-click pytest-django
+poetry add typer
 
 pre-commit install
