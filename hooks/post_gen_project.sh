9359b74c74c57c7a1b97976520db5826bd0b54ab
James Turk <dev@jamesturk.net>
2023-12-25 02:57:32 -0600

    pre-commit

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# the latest versions of our core packages
 poetry add --dev pytest pytest-cov black mkdocs-material ruff mypy
 poetry add structlog
 poetry add typer
+
+pre-commit install
