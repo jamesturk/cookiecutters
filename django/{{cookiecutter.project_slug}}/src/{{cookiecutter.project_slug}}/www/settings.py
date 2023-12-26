0a02426d9a04cb19571fdacce56725dde088d591
James Turk <dev@jamesturk.net>
2023-12-26 00:18:59 -0600

    alter settings for env

--- django/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/www/settings.py
+++ django/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/www/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import environ
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
+env.read_env(BASE_DIR / ".env")
 
 # Read Checklist Before Deploying
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -77,7 +78,7 @@ WSGI_APPLICATION = "{{cookiecutter.project_slug}}.www.wsgi.application"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DATABASES = {
-    "default": env.db(),
+    "default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3"),
 }
 
 
